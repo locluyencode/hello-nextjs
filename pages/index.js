d66abf60f34a1c0f1b697bcef3399864803fc44c
locluyencode <81458734+locluyencode@users.noreply.github.com>
2021-12-03 22:07:09 +0700

    ok

--- pages/index.js
+++ pages/index.js
@@ -22,18 +22,18 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="https://www.facebook.com/baloc.nguyen.14/" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://www.facebook.com/baloc.nguyen.14/" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://www.facebook.com/baloc.nguyen.14/"
             className={styles.card}
           >
             <h2>Examples &rarr;</h2>
@@ -41,7 +41,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://www.facebook.com/baloc.nguyen.14/"
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
@@ -54,7 +54,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.facebook.com/baloc.nguyen.14/"
           target="_blank"
           rel="noopener noreferrer"
         >
